c008f05e5da670b31bb3528fb8589729c1006dc6
Jroslav85 <jroslav85@gmail.com>
2024-07-04 23:25:42 +0500

    change class CommandParser
    added class CommandGenerate.

--- src/main/java/com/jroslav/passj/parser/CommandParser.java
+++ src/main/java/com/jroslav/passj/parser/CommandParser.java
@@ -3,15 +3,19 @@ package com.jroslav.passj.parser;
 import java.util.Map;
 
 import com.jroslav.passj.command.Command;
+import com.jroslav.passj.command.CommandGenerate;
 import com.jroslav.passj.command.CommandHelp;
 
 public class CommandParser {
 	private final Map<String, Command> commands;
 	private final Command commandHelp = new CommandHelp();
+	private final Command commandGenerate = new CommandGenerate();
 
 	public CommandParser() {
 		commands = Map.of(commandHelp.getStringRepresentation(), commandHelp,
-				commandHelp.getShortStringRepresentation(), commandHelp);
+				commandHelp.getShortStringRepresentation(), commandHelp,
+				commandGenerate.getStringRepresentation(), commandGenerate,
+				commandGenerate.getShortStringRepresentation(), commandGenerate);
 	}
 
 	public void parse(final String... strings) {
